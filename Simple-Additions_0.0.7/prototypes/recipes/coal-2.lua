d6a737b478d3e40b41be37e63e56accb85892c0c
The-Saus <The-Saus@users.noreply.github.com>
2019-06-28 11:20:54 -0500

    Update coal-2.lua

--- Simple-Additions_0.0.7/prototypes/recipes/coal-2.lua
+++ Simple-Additions_0.0.7/prototypes/recipes/coal-2.lua
@@ -2,9 +2,10 @@ data:extend({
   {
     type = "recipe",
     name = "coalx2",
-    category = "chemistry",
+    subgroup = "fluid-recipes",
     energy_required = 3,
     enabled = true,
+	order = "b[fluid-chemistry]-f[solid-fuel-from-heavy-oil]",
     ingredients =
     {
       {type="item", name="coal", amount=1}
